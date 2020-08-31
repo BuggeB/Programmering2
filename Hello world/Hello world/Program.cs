ebef1b2ef079b2090288c523ff1ee352455e8980
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-08-31 13:38:02 +0200

    Läxa färdig
    pee pee poo poo

--- Hello world/Hello world/Program.cs
+++ Hello world/Hello world/Program.cs
@@ -6,7 +6,28 @@ namespace Hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Enter username:");
+
+           
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+            Console.WriteLine("Lever du: yes/no");
+            var answer = Console.ReadLine();
+            if (answer == "yes")
+            {
+                Console.WriteLine("Bra att du lever");
+            }
+            else
+            {
+                Console.WriteLine("Synd att du är död... pee pee poo poo");
+            }
+
+
         }
     }
 }
