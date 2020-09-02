91669986683de5feccb148f5650a3656e489d2e3
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-09-02 12:18:07 +0200

    LäxaTorsdag
    Färdig

--- Hello world/Hello world/Program.cs
+++ Hello world/Hello world/Program.cs
@@ -4,28 +4,65 @@ namespace Hello_world
 {
     class Program
     {
+        public static object Control { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
+            while(true)
+            {
+                Console.WriteLine("Ange en siffra mellan 1-2");
+
+                int choose = Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine("stinky");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("poopey");
+                        break;
+
+                    default:
+                        Console.WriteLine("Segt du får skriva 1 eller 2");              
+                        break;
+
+                }
+
+                Console.WriteLine("Skriv sitt namn:");
 
            
             string userName = Console.ReadLine();
-            Console.WriteLine("Username is: " + userName);
 
-            Console.WriteLine("Enter your age:");
+            Console.WriteLine("Du heter: " + userName);
+               
+            Console.WriteLine("Skriv din ålder:");
             int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is: " + age);
+            Console.WriteLine("Din ålder är: " + age);
+                for (int i = 0; i < 18; i++)
+                {
+                    Console.WriteLine("Du är inte myndig skitunge -.-");
+                    break;
+                }
 
-            Console.WriteLine("Lever du: yes/no");
+                Console.WriteLine("Lever du: ja/nej");
             var answer = Console.ReadLine();
-            if (answer == "yes")
+            if (answer == "ja")
             {
                 Console.WriteLine("Bra att du lever");
             }
             else
             {
                 Console.WriteLine("Synd att du är död... pee pee poo poo");
+                    break;
+                    
             }
+            }
+
+            
+
+           
 
 
         }
