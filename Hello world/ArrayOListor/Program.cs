c1dadb6590bbbd66a01ad9460f1e97a91a3effd5
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-09-03 10:38:34 +0200

    ArrayListaFärdig
    Pee pee

---
+++ Hello world/ArrayOListor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayOListor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[5];
+
+            var temp = names;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            
+            Array.Reverse(temp);
+            foreach (var name in temp)
+            {
+                Console.Write(name + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
