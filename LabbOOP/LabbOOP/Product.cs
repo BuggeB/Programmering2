f7f0752a364258c15da621502f4a07dbbb0ae1aa
BuggeB <54797055+BuggeB@users.noreply.github.com>
2020-10-15 10:13:57 +0200

    Polimorfi läxa färdig

--- LabbOOP/LabbOOP/Product.cs
+++ LabbOOP/LabbOOP/Product.cs
@@ -10,4 +10,14 @@ namespace LabbOOP
         public string _dog = "Dog";
 
     }
+    class Liquid
+    {
+        public string _cola = "Cola";
+        public string _nocco = "Nocco";
+    }
+    class Snacks
+    {
+        public string _chips = "Chips";
+        public string _popcorn = "Popcorn";
+    }
 }
--- LabbOOP/LabbOOP/Program.cs
+++ LabbOOP/LabbOOP/Program.cs
@@ -15,6 +15,9 @@ namespace LabbOOP
 
 
             Product products = new Product();
+            Liquid liquid = new Liquid();
+            Snacks snacks = new Snacks();
+
 
             Boolean case3 = true;
 
@@ -22,7 +25,11 @@ namespace LabbOOP
             {
                 Console.WriteLine("Click 1 to choose Apple");
                 Console.WriteLine("Click 2 to choose Dog");
-                Console.WriteLine("Click 3 to exit");
+                Console.WriteLine("Click 3 to choose Cola");
+                Console.WriteLine("Click 4 to choose Nocco");
+                Console.WriteLine("Click 5 to choose Chips");
+                Console.WriteLine("Click 6 to choose Popcorn");
+                Console.WriteLine("Click 7 to exit");
                 var customerInput = Convert.ToInt32(Console.ReadLine());
 
                 switch (customerInput)
@@ -38,6 +45,26 @@ namespace LabbOOP
                         break;
 
                     case 3:
+                        customer1._cart.Add(liquid._cola);
+
+                        break;
+
+                    case 4:
+                        customer1._cart.Add(liquid._nocco);
+
+                        break;
+
+                    case 5:
+                        customer1._cart.Add(snacks._chips);
+
+                        break;
+
+                    case 6:
+                        customer1._cart.Add(snacks._popcorn);
+
+                        break;
+
+                    case 7:
                         case3 = false;
                         Console.WriteLine("Exiting...");
                         break;
